5b59a68220497b4ecf77a82eb90b0af46fb2bb2c
Graycie-X <49578879+Graycie-X@users.noreply.github.com>
2021-08-11 12:46:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function spam() {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://futtobecom.stars.ne.jp/3ds/Codes/POST');
     xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
-    xhr.send('Area[0]=荒らし&Area[1]=荒らし&Area[2]=0&Area[3]=0&Area[4]=荒らし');
+    xhr.send('Area[0]=B1の光&Area[1]=B1&Area[2]=0&Area[3]=0&Area[4]=B1の光公式ホームページ http://b1shine.iiyudana.net/');
 }
 
 function spamming() {
